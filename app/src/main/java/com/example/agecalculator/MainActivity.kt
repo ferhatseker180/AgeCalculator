3aedf87af0f077f2f21dfd9fe378a0725aa2b162
ferhatseker180 <ferhatseker180@gmail.com>
2023-04-18 19:44:57 +0300

    TimePicker Ve DatePicker kullanılarak yaş hesaplaması dakika,saat ve gün cinsinden hesaplandı.

---
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.agecalculator
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSelect.setOnClickListener {
+
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+     val dpd =   DatePickerDialog(this,
+             DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                 val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                 textViewDate.text = selectedDate.toString()
+                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                 val theDate = sdf.parse(selectedDate)
+                 theDate?.let {
+                     val convertToMinute = theDate.time / 60000
+                     val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                     currentDate?.let {
+                         val currentDateToMinute = currentDate.time / 60000
+                         val minus = currentDateToMinute - convertToMinute
+                         val hours = minus/60
+                         val day = hours / 24
+                         textViewHours.text = hours.toString()
+                         textViewTime.text = minus.toString()
+                         textViewDay.text = day.toString()
+                     }
+
+                 }
+
+             },
+            year,
+            month,
+            day
+            )
+        dpd.datePicker.maxDate = System.currentTimeMillis()-86400000
+            dpd.show()
+
+
+
+    }
+}
